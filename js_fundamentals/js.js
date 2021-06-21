4abac9ae51c82d9e45d31503e162b1df284fd6c0
Lucas Martins <lucasshc@gmail.com>
2021-06-21 13:06:56 -0300

    update ex e portfolio css

---
+++ js_fundamentals/js.js
@@ -0,0 +1,8 @@
+let nomeVelho = prompt("Qual o nome do ancião?")
+let idadeVelho = prompt("Qual a idade do ancião?")
+let nomeNovo = prompt("Qual o nome da criança?")
+let idadeNovo = prompt("Qual a idade da criança?")
+let difIdade = idadeVelho - idadeNovo
+window.alert(`O nome do ancião é ${nomeVelho} e tem ${idadeVelho} anos`)
+window.alert(`O nome da criança é ${nomeNovo} e tem ${idadeNovo} anos`)
+window.alert(`A diferença de idade entre os dois é de ${difIdade} anos`)
